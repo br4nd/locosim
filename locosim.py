99af62914da99f8686fee9bab783dfa030fa5e3b
br4nd <brandondewberry@gmail.com>
2016-03-20 21:56:34 -0500

    version 2

--- locosim.py
+++ locosim.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Usage:
+>>> python locosim.py
 
+prompts user to choose parameters.csv in a folder
+creates saved.pkl and plots.pdf in the same folder
+
+Originally created Feb 2016
+@author: B. Dewberry
+Feb 21 2016: changed name to qualify.py, added qualify-info.csv
+Version 2, Mar 20 2016 :
+- fixed geometry inversion bug
+- queries user for <parameters>.csv file
+- reads config parameters from <parameters>.csv file in any folder
+- stores saved.pkl in same folder as <parameters>.csv
+- stores plots.pkl in same folder as <parameters>.csv
+"""
 import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
 from numpy import dot, transpose, diag
 from numpy.linalg import inv, norm
@@ -12,55 +30,118 @@ import sys, os
 sys.path.insert(0,os.path.abspath('..'))
 from solvers.nls import nls_m1_a4_3d
 
-import pdb
+import pdb, csv
+from pprint import pprint as pp
+
+
+config_file = 'locosim_input_parameters.csv'
 
 #-------------------------------------------------------------------------------
-def set_config() :
+def get_config(config_file) :
     # TODO read from .csv file
 
-    # Anchor locations
-    #                x, y, z
-    A = np.array([ [ 4,-5, 1],   # A0
-                   [ 4, 4, 2],   # A1
-                   [-5, 4, 1],   # A2
-                   [-4,-4, 2] ]) # A3
+    if config_file == 'test' :
+        # Anchor locations
+        #                x, y, z
+        A = np.array([ [ 4,-5, 1],   # A0
+                       [ 4, 4, 2],   # A1
+                       [-5, 4, 1],   # A2
+                       [-4,-4, 2] ]) # A3
 
-    # Add random noise (row,col)
-    #Rstd = [0.024, 0.055, 0.056, 0.239]
-    rstd = [.025, .025, .025, .025]
-    #Rbias = [-0.002, -0.002, -0.002, -0.002]  # range bias
-    rbias = [0.01, 0.01, 0.01, 0.01]  # range bias
+        # Add random noise (row,col)
+        #Rstd = [0.024, 0.055, 0.056, 0.239]
+        rstd = [.025, .025, .025, .025]
+        #Rbias = [-0.002, -0.002, -0.002, -0.002]  # range bias
+        rbias = [0.01, 0.01, 0.01, 0.01]  # range bias
 
-    # x_vec = slice(-4, 4+dx, dx);
-    # y_vec = slice(-4, 4+dy, dy);
-    dx = 0.5
-    #dx = 1
-    xlim = [-15,15]
-    nx = int((xlim[1]-xlim[0])/dx) + 1
-    xvec = np.linspace(xlim[0],xlim[1],nx,endpoint=True)
+        # x_vec = slice(-4, 4+dx, dx);
+        # y_vec = slice(-4, 4+dy, dy);
+        dx = 0.5
+        #dx = 1
+        xlim = [-15,15]
+        nx = int((xlim[1]-xlim[0])/dx) + 1
+        xvec = np.linspace(xlim[0],xlim[1],nx,endpoint=True)
 
-    dy = 0.5
-    #dy = 1
-    ylim = [-7,7]
-    ny = int((ylim[1]-ylim[0])/dy) + 1
-    yvec = np.linspace(ylim[0],ylim[1],ny,endpoint=True)
+        dy = 0.5
+        #dy = 1
+        ylim = [-7,7]
+        ny = int((ylim[1]-ylim[0])/dy) + 1
+        yvec = np.linspace(ylim[0],ylim[1],ny,endpoint=True)
 
-    zt = -1.0
-    # dz = 0.25
-    # zlim = [0,0]
-    # nz = int((zlim[1]-zlim[0])/dz) + 1
-    # zvec = np.linspace(zlim[0],zlim[1],dz,endpoint=True)
-    # if not zvec : # if empty
-    #     zvec = 0
+        zt = -1.0
 
-    ntrials = 5
+        blur = 'none'
 
-    config = { 'A': A,
-               'xvec': xvec, 'yvec': yvec, 'zt': zt,
-               'rstd': rstd, 'rbias': rbias,
-               'ntrials': ntrials }
+        ntrials = 5
 
-    return config
+        config = { 'A': A,
+                   'xvec': xvec,
+                   'yvec': yvec,
+                   'zt': zt,
+                   'rstd': rstd,
+                   'rbias': rbias,
+                   'ntrials': ntrials,
+                   'blur' : blur }
+
+        return config
+
+    elif os.path.isfile(config_file) :
+        #--- read config from file
+        print 'Reading config parameters from %s.' % config_file
+        A = []
+
+        with open(config_file,'rb') as csvfile :
+            #    stuff = csv.reader(csvfile,delimiter=' ',quotechar='|')
+            reader = csv.reader(csvfile)
+            for row in reader :
+                #print ', '.join(row)
+                if row[0] == '' :
+                    continue
+                if 'A' == row[0][0] :
+                    if 'Anchors' == row[0] :
+                        continue
+                    A.append([ float(row[1]), float(row[2]), float(row[3]) ])
+                elif 'rstd' == row[0] :
+                    rstd = map(float,row[1:])
+                elif 'rbias' == row[0] :
+                    rbias = map(float,row[1:])
+                elif 'dx' == row[0] :
+                    dx = float(row[1])
+                elif 'dy' == row[0] :
+                    dy = float(row[1])
+                elif 'xlim' == row[0] :
+                    xlim = map(float,row[1:3])
+                elif 'ylim' == row[0] :
+                    ylim = map(float,row[1:3])
+                elif 'zt' == row[0] :
+                    zt = float(row[1])
+                elif 'ntrials' == row[0] :
+                    ntrials = int(row[1])
+#                elif 'blur' == row[0] :
+#                    blur = row[1]
+
+            nx = int((xlim[1]-xlim[0])/dx) + 1
+            xvec = np.linspace(xlim[0],xlim[1],nx,endpoint=True)
+            ny = int((ylim[1]-ylim[0])/dy) + 1
+            yvec = np.linspace(ylim[0],ylim[1],ny,endpoint=True)
+
+            config = { 'A': np.array(A),
+                       'xvec': xvec,
+                       'yvec': yvec,
+                       'zt': zt,
+                       'rstd': rstd,
+                       'rbias': rbias,
+                       'ntrials': ntrials,
+#                       'blur' : blur }
+                      }
+            pp(config)
+
+            return config
+
+    #pdb.set_trace()
+
+    else :
+        raise ValueError('No config file found named %s.' % config_file)
 
 #-------------------------------------------------------------------------------
 def monte_carlo_2d(config) :
@@ -79,6 +160,8 @@ def monte_carlo_2d(config) :
     Estd = np.nan*np.ones((nx,ny))
     Emean_x = np.nan*np.ones((nx,ny))
     Estd_x = np.nan*np.ones((nx,ny))
+    Emean_y = np.nan*np.ones((nx,ny))
+    Estd_y = np.nan*np.ones((nx,ny))
     Emean_z = np.nan*np.ones((nx,ny))
     Estd_z = np.nan*np.ones((nx,ny))
 
@@ -109,6 +192,8 @@ def monte_carlo_2d(config) :
             #pdb.set_trace()
             Emean_x[ii,jj] = norm(Mh_mean[0]-Mt[0])
             Estd_x[ii,jj] = np.std(Mh[:,0])
+            Emean_y[ii,jj] = norm(Mh_mean[1]-Mt[1])
+            Estd_y[ii,jj] = np.std(Mh[:,1])
             Emean_z[ii,jj] = norm(Mh_mean[2]-Mt[2])
             Estd_z[ii,jj] = np.std(Mh[:,2])
             #print 'xt: %3.2f, yt: %3.2f' % (xt,yt)
@@ -122,6 +207,7 @@ def monte_carlo_2d(config) :
 
     D = {'Emean': Emean, 'Estd': Estd,
         'Emean_x': Emean_x, 'Estd_x': Estd_x,
+        'Emean_y': Emean_y, 'Estd_y': Estd_y,
         'Emean_z': Emean_z, 'Estd_z': Estd_z}
     return D
 
@@ -145,116 +231,144 @@ def load_data(fn) :
 
 #-------------------------------------------------------------------------------
 if __name__ == '__main__' :
+
     import time
     t0 = time.time()
 
-    # comment this line to run monte carlo.  read stored otherwise
-    #fn = 'test.pkl'
-    if not 'fn' in locals() :
-        ####################  Read config parameters
-        # TODO: prompt user for ilename.csv file
-        # or, if .pkl selected, skip over sim and read/plot stored data
-        config = set_config()
+    ffn_config = []
+    #ffn_config = './test2/parameters.csv'
 
-        ####################  Run sim
-        D = monte_carlo_2d(config)
-        t1 = time.time() - t0
-        print 't1: %5.2f' % t1
+    if not ffn_config :
+        import Tkinter, tkFileDialog
+        prompt = 'Choose an config file'
+        types = [('All files','*'),('csv','*.csv')]
+        defaultextension = '*'
+        root = Tkinter.Tk()
+        root.withdraw() # don't want a full GUI
+        root.update()
+        ffn_config = tkFileDialog.askopenfilename(parent=root,title=prompt,filetypes=types)
 
-        # TODO try gdop analysis
+    path,fn_config = os.path.split(ffn_config)
+    fn_main,fn_ext = os.path.splitext(fn_config)
+    fn_pkl = 'saved.pkl'
+    ffn_pkl = os.path.join(path,fn_pkl)
 
-        ####################  Store sim results
-        # TODO: store in filename.pkl
-        fn = 'test.pkl'
-        store_data(fn,config,D)
+    fn_pdf = 'plots.pdf'
+    ffn_pdf = os.path.join(path,fn_pdf)
+    pdf = PdfPages(ffn_pdf)
 
-    config,D = load_data(fn)
+    #--- Read config parameters
+    #config = get_config('test')
+    config = get_config(ffn_config)
+
+    #------------------  Run sim
+    D = monte_carlo_2d(config)
+    t1 = time.time() - t0
+
+    # TODO try gdop analysis
+
+    #------------------- Store sim results
+    store_data(ffn_pkl,config,D)
+
+    #------------------- Compute stats
+    config,D = load_data(ffn_pkl)
     Emean = np.transpose(D['Emean'])
     Estd = np.transpose(D['Estd'])
     Emean_x = np.transpose(D['Emean_x'])
     Estd_x = np.transpose(D['Estd_x'])
+    Emean_y = np.transpose(D['Emean_y'])
+    Estd_y = np.transpose(D['Estd_y'])
     Emean_z = np.transpose(D['Emean_z'])
     Estd_z = np.transpose(D['Estd_z'])
 
+    #-------------------- Plot stuff
     X,Y = np.meshgrid(config['xvec'],config['yvec'])
 
-    ####################  Plot sim results - 3D
     from mpl_toolkits.mplot3d import axes3d
-    myshading = 'gourard'
-    myshading = 'none'
+    #shading = 'gourard'
+    shading = 'none'
+
     plt.ion()
+    #---------------  Plot sim results - 3D
 #    fig = plt.figure(figsize=(7,5),dpi=150)
     fig = plt.figure()
     ax = fig.add_subplot(111,projection='3d')
     #X, Y, Z = axes3d.get_test_data(0.05)
     #ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3)
-    #pdb.set_trace()
-    cset = ax.contourf(X, Y, Estd, zdir='z', offset=zt, cmap='hot',
-            levels=np.linspace(0,Estd.max(),100))
 
-    plt.suptitle('MonteCarlo n:%d, rstd:%3.3f, rbias:%3.3f'
+    zt = config['zt']
+    cset = ax.contourf(X, Y, Estd, zdir='z', offset=zt, cmap='hot',shading=shading,levels=np.linspace(0,Estd.max(),100))
+
+    plt.suptitle('MonteCarlo n:%d, rstd:%.3f, rbias:%.3f'
         % (config['ntrials'], max(config['rstd']), max(config['rbias'])))
-
-    #ax1 = fig.gca(projection='3d')
     A = config['A']
-    ax.scatter(A[:,0],A[:,1],A[:,2],s=20,c='blue',marker='d',edgecolors='lightblue',label='Anchors')
-    ax.set_zlim([0, 3])
-    ax.set_xlabel('x (m)'); ax.set_ylabel('y (m)'); ax.set_zlabel('z (m)')
-#    ax.axis_equal(True)
+    ax.scatter(A[:,0],A[:,1],A[:,2],s=25,c='blue',marker='d',edgecolors='blue',label='Anchors')
+    ax.set_zlim([0, max(A[:,2])+1])
 
-    ####################  Plot sim results - Estd
-#    fig = plt.figure(figsize=(7,14),dpi=150)
+    #-------------------- Plot slant range errors
+    #fig = plt.figure(figsize=(7,14),dpi=150)
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
-    plt.pcolormesh(config['xvec'],config['yvec'],Emean,cmap='hot',shading=myshading,vmin=0,vmax=np.abs(Emean).max())
-    #plt.suptitle('Plan View')
-    plt.title('Emean')
+    plt.pcolormesh(config['xvec'],config['yvec'],Emean,cmap='hot',shading=shading,vmin=0,vmax=np.abs(Emean).max())
+    plt.title('Ebias (max=%.7f)' % np.max(Emean))
     plt.colorbar()
-    ax1.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='lightblue',label='Anchors')
+    ax1.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='blue',label='Anchors')
 
     ax2 = fig.add_subplot(212)
-    plt.pcolormesh(config['xvec'],config['yvec'],Estd,cmap='hot',shading=myshading,vmin=0,vmax=Estd.max())
-    plt.title('Estd')
+    plt.pcolormesh(config['xvec'],config['yvec'],Estd,cmap='hot',shading=shading,vmin=0,vmax=Estd.max())
+    plt.title('Estd (max=%.7f)' % np.max(Estd))
     plt.colorbar()
-    ax2.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='lightblue',label='Anchors')
-    plt.suptitle('MonteCarlo n:%d, rstd:%3.3f, rbias:%3.3f'
-        % (config['ntrials'], max(config['rstd']), max(config['rbias'])))#
+    ax2.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='blue',label='Anchors')
 
-    # Add figure for x, y, and z errors
+#    plt.suptitle('MonteCarlo n:%d, max(emean):%.3fcm, max(estd):%.3fcm'
+#        % (config['ntrials'], np.max(Emean)*100., np.max(Estd)*100.))#
+
+    #-------------------- Plot sim results - Estd
     maxv = np.max([Emean_x.max(),Estd_x.max(),Emean_z.max(),Estd_z.max()])
     fig = plt.figure()
 
     ax1 = fig.add_subplot(321)
-    plt.pcolormesh(config['xvec'],config['yvec'],Emean_x,cmap='hot',shading=myshading,vmin=0,vmax=np.abs(Emean_x).max())
-    plt.title('Emean_x, max=%3.3f' % np.abs(Emean_x).max())
+    plt.pcolormesh(config['xvec'],config['yvec'],Emean_x,cmap='hot',shading=shading,vmin=0,vmax=np.abs(Emean_x).max())
+    #pdb.set_trace()
+    plt.title('Emean_x, max=%.7f' % np.abs(Emean_x).max())
     plt.colorbar()
-    ax1.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='lightblue',label='Anchors')
+    ax1.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='blue',label='Anchors')
 
     ax2 = fig.add_subplot(322)
-    plt.pcolormesh(config['xvec'],config['yvec'],Estd_x,cmap='hot',shading=myshading,vmin=0,vmax=np.abs(Estd_x).max())
-    plt.title('Estd_x, max=%3.3f' % np.abs(Estd_x).max())
+    plt.pcolormesh(config['xvec'],config['yvec'],Estd_x,cmap='hot',shading=shading,vmin=0,vmax=np.abs(Estd_x).max())
+    plt.title('Estd_x, max=%.7f' % np.abs(Estd_x).max())
     plt.colorbar()
-    ax2.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='lightblue',label='Anchors')
+    ax2.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='blue',label='Anchors')
 
-    ax = fig.add_subplot(325)
-    plt.pcolormesh(config['xvec'],config['yvec'],Emean_z,cmap='hot',shading=myshading,vmin=0,vmax=np.abs(Emean_z).max())
-    plt.title('Emean_z, max=%3.3f' % np.abs(Emean_z).max())
+    ax3 = fig.add_subplot(323)
+    plt.pcolormesh(config['xvec'],config['yvec'],Emean_y,cmap='hot',shading=shading,vmin=0,vmax=np.abs(Emean_y).max())
+    plt.title('Emean_y, max=%.7f' % np.abs(Emean_y).max())
     plt.colorbar()
-    ax.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='lightblue',label='Anchors')
+    ax3.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='blue',label='Anchors')
 
-    ax = fig.add_subplot(326)
-    plt.pcolormesh(config['xvec'],config['yvec'],Estd_z,cmap='hot',shading=myshading,vmin=0,vmax=np.abs(Estd_z).max())
-    plt.title('Estd_z, max=%3.3f' % np.abs(Estd_z).max())
+    ax4 = fig.add_subplot(324)
+    plt.pcolormesh(config['xvec'],config['yvec'],Estd_y,cmap='hot',shading=shading,vmin=0,vmax=np.abs(Estd_y).max())
+    plt.title('Estd_y, max=%.7f' % np.abs(Estd_y).max())
     plt.colorbar()
-    ax.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='lightblue',label='Anchors')
+    ax4.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='blue',label='Anchors')
+
+    ax5 = fig.add_subplot(325)
+    plt.pcolormesh(config['xvec'],config['yvec'],Emean_z,cmap='hot',shading=shading,vmin=0,vmax=np.abs(Emean_z).max())
+    plt.title('Emean_z, max=%.7f' % np.abs(Emean_z).max())
+    plt.colorbar()
+    ax5.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='blue',label='Anchors')
+
+    ax6 = fig.add_subplot(326)
+    plt.pcolormesh(config['xvec'],config['yvec'],Estd_z,cmap='hot',shading=shading,vmin=0,vmax=np.abs(Estd_z).max())
+    plt.title('Estd_z, max=%.7f' % np.abs(Estd_z).max())
+    plt.colorbar()
+    ax6.scatter(A[:,0],A[:,1],s=20,c='blue',marker='d',edgecolors='blue',label='Anchors')
 
     # TODO: store as pdf
 
-
 #    plt.show()
 
-
-    t2 = time.time() - t0
-    print 't2: %5.2f' % t2
+    print '  compute time: %5.3f' % t1
+    print '  total time: %5.3f' % (time.time() - t0)
 
     pdb.set_trace()
